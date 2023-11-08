4e2500b7a92529e82cce9ef2fcca29a288041a68
Greg Graham <greg@grahamtx.net>
2023-11-08 11:24:30 -0600

    Complete working version

--- combat.go
+++ combat.go
@@ -1,8 +1,40 @@
 package main
 
-import "techinar/player"
+import (
+	"fmt"
+	"techinar/player"
+)
 
-func main() {
+func combat() int {
 	p1 := player.New("Joe", 10, 10, 8)
 	p2 := player.New("Bob", 10, 8, 10)
+	p1Lives := true
+	p2Lives := true
+
+	for p1Lives && p2Lives {
+		p2Lives = p1.Attack(p2)
+		if !p2Lives {
+			break
+		}
+		p1Lives = p2.Attack(p1)
+	}
+	if p1Lives {
+		return 1
+	} else {
+		return 2
+	}
+}
+
+func main() {
+	p1Count := 0
+	p2Count := 0
+	for i := 0; i < 1000; i++ {
+		if combat() == 1 {
+			p1Count++
+		} else {
+			p2Count++
+		}
+	}
+	fmt.Printf("Player 1 won %d times.\n", p1Count)
+	fmt.Printf("Player 2 won %d times.\n", p2Count)
 }
