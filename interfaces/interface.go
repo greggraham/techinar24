f0de65a8b2a519ffc218b40712819cbc4683f25a
Greg Graham <greg@grahamtx.net>
2023-11-28 10:06:26 -0600

    Add interface example

---
+++ interfaces/interface.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type MyInterface interface {
+	MethodWithoutParameters()
+	MethodWithParameter(float64)
+	MethodWithReturnValue() string
+}
+
+type MyType int
+
+func (m MyType) MethodWithoutParameters() {
+	fmt.Println("MethodWithoutParameters called")
+}
+
+func (m MyType) MethodWithParameter(f float64) {
+	fmt.Println("MethodWithParameter called with", f)
+}
+
+func (m MyType) MethodWithReturnValue() string {
+	return "Hi from MethodWithReturnValue"
+}
+
+func (m MyType) MethodNotInInterface() {
+	fmt.Println("MethodNotInInterface called")
+}
+
+func main() {
+	var value MyInterface
+	value = MyType(5)
+	value.MethodWithoutParameters()
+	value.MethodWithParameter(127.3)
+	fmt.Println(value.MethodWithReturnValue())
+}
