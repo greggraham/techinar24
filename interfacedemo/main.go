83344a793cd7d2306af34da3c21efee48a781084
Greg Graham <greg@grahamtx.net>
2023-11-30 10:08:07 -0600

    Add command line arg

--- interfacedemo/main.go
+++ interfacedemo/main.go
@@ -1,7 +1,31 @@
 package main
 
+import (
+	"os"
+	"strconv"
+)
+
 func main() {
-	//demo1()
-	//demo2()
-	demo3()
+	var d int
+	var err error
+	if len(os.Args) < 2 {
+		d = 0
+	} else {
+		d, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			d = 0
+		}
+	}
+	switch d {
+	case 1:
+		demo1()
+	case 2:
+		demo2()
+	case 3:
+		demo3()
+	default:
+		demo1()
+		demo2()
+		demo3()
+	}
 }
